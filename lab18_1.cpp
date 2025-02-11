7fdee911b620f069e39947bcd4e7436de11334d6
zewexqa <154309211+zewexqa@users.noreply.github.com>
2025-02-11 18:38:41 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x, y, w, h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1, Rect R2) {
+    double x_overlap = max(0.0, min(R1.x + R1.w, R2.x + R2.w) - max(R1.x, R2.x));
+    double y_overlap = max(0.0, min(R1.y, R2.y) - max(R1.y - R1.h, R2.y - R2.h));
+    return x_overlap * y_overlap;
 }
+
